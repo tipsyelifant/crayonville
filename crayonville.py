9904c2580edefacc881c75f72e52410c4fe4718e
tipsyelifant <deirepromissio@gmail.com>
2024-05-24 14:32:31 +0000

    increase bottom padding to 2 rem

--- crayonville.py
+++ crayonville.py
@@ -43,7 +43,7 @@ def run():
         <style>
                .block-container {
                     padding-top: 1rem;
-                    padding-bottom: 1rem;
+                    padding-bottom: 2rem;
                     padding-left: 1rem;
                     padding-right: 1rem;
                 }
