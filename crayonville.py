aae43abf2fda465f40b94ade716ff922555ba769
tipsyelifant <deirepromissio@gmail.com>
2024-07-05 11:42:55 +0800

    Update to perceptive purple-5.svg

--- crayonville.py
+++ crayonville.py
@@ -488,7 +488,7 @@ def run():
             "image": "boldbrown.png"
         },
         frozenset(["Blue", "Red"]): {
-            "image": "perceptivepurple-4.svg"
+            "image": "perceptivepurple-5.svg"
         },
         
     }
