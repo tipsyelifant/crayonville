238da743267c6952aae741f34f0b182beb0f9d10
tipsyelifant <deirepromissio@gmail.com>
2024-08-12 10:57:44 +0800

    Update crayonville.py

--- crayonville.py
+++ crayonville.py
@@ -557,13 +557,15 @@ def run():
     elif st.session_state.progress == 1:
         st.markdown('<div style="font-size:20px;"><b>Step 1:</b> Click on the link and fill up the FormSG</div>', unsafe_allow_html=True)
         st.write("https://go.gov.sg/crayonvilleregistration") # Change here to show your link
-        st.markdown('<div style="font-size:16px;"><b>Important:</b> Please complete the FormSG to ensure accurate data collection for the profiling quiz. Please do not sharing your quiz code.</div>', unsafe_allow_html=True)
+        st.markdown('<div style="font-size:15px;"><b>Important:</b> Please complete the FormSG to ensure accurate data collection for the profiling quiz. Please do not sharing your quiz code.</div>', unsafe_allow_html=True)
+        st.markdown('') # Empty string for spacing
         st.markdown('') # Empty string for spacing
         st.markdown('<div style="font-size:20px;"><b>Step 2:</b> You will receive an email with the code after you have completed the FormSG</div>', unsafe_allow_html=True)
         st.markdown('') # Empty string for spacing
+        st.markdown('') # Empty string for spacing
         st.markdown('<div style="font-size:20px;"><b>Step 3:</b> Enter the code from your email! <b>Do not copy and paste!</b></div>', unsafe_allow_html=True)
         password = st.text_input('', label_visibility='collapsed')
-        st.markdown('<div style="font-size:16px;"><b>Tip:</b> If you encounter an error message stating that the code is incorrect even though you typed it accurately, try pressing "Enter" on your keyboard before clicking the "Start Personality Quiz" button. This might help ensure the code is registered properly.</div>', unsafe_allow_html=True)
+        st.markdown('<div style="font-size:15px;"><b>Tip:</b> If you encounter an error message stating that the code is incorrect even though you typed it accurately, try pressing "Enter" on your keyboard before clicking the "Start Personality Quiz" button. This might help ensure the code is registered properly.</div>', unsafe_allow_html=True)
         st.markdown("")
         
         if st.button("Start Personality Quiz", type="primary"):
