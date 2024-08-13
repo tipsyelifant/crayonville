6e7d26a2262762fd9cc8a3d73705623b6d433705
tipsyelifant <deirepromissio@gmail.com>
2024-08-13 10:08:55 +0800

    Update registration page

--- crayonville.py
+++ crayonville.py
@@ -556,12 +556,11 @@ def run():
             
     elif st.session_state.progress == 1:
         st.markdown('<div style="font-size:15px;"><b>Important:</b> Please complete the FormSG to ensure accurate data collection for the profiling quiz. Please do not sharing your quiz code.</div>', unsafe_allow_html=True)
+        st.markdown('') # Empty string for spacing
         st.markdown('<div style="font-size:20px;"><b>Step 1:</b> Click on the link and fill up the FormSG</div>', unsafe_allow_html=True)
         st.write("https://go.gov.sg/crayonvilleregistration") # Change here to show your link
         st.markdown('') # Empty string for spacing
-        st.markdown('') # Empty string for spacing
         st.markdown('<div style="font-size:20px;"><b>Step 2:</b> You will receive an email with the code after you have completed the FormSG</div>', unsafe_allow_html=True)
-        st.markdown('') # Empty string for spacing
         st.markdown('') # Empty string for spacing
         st.markdown('<div style="font-size:20px;"><b>Step 3:</b> Enter the code from your email! <b>Do not copy and paste!</b></div>', unsafe_allow_html=True)
         password = st.text_input('', label_visibility='collapsed')
